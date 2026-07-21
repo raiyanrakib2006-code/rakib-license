9eb47a5dba50a6df473cf066e22e91c6b7900939
raiyanrakib2006-code <raiyanrakib2006-code@users.noreply.github.com>
2026-07-21 08:10:51 +0600

    Install final PowerShell license system

--- config.js
+++ config.js
@@ -1,7 +1,1 @@
-window.RAKIB_LICENSE_CONFIG = {
-  displayText: "Rakib (Demo)",
-  sessionMinutes: 60,
-  licenseFile: "licenses.json",
-  mainScript: "main.js",
-  title: "Rakib License Login"
-};
+window.RAKIB_LICENSE_CONFIG={displayText:"Rakib (Demo)",licenseFile:"licenses.json",mainScript:"main.js",title:"Rakib License Login"};
