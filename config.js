bf4ac8d9365b663e0e705f1463bae184f91d5c70
raiyanrakib2006-code <raiyanrakib2006-code@users.noreply.github.com>
2026-07-21 08:27:40 +0600

    Set Live demo display

--- config.js
+++ config.js
@@ -1,1 +1,1 @@
-window.RAKIB_LICENSE_CONFIG={displayText: "Live",licenseFile:"licenses.json",mainScript:"main.js",title: "Rahat License Login"};
+window.RAKIB_LICENSE_CONFIG={displayText:"Live",licenseFile:"licenses.json",mainScript:"main.js",title:"Live License Login"};
